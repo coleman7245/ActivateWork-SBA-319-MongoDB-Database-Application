d7f0b5a38d2b8b2e4f721d3c6b0b29171423b894
coleman7245 <dlcoleman1@live.com>
2025-01-31 11:13:12 -0700

    Added the MongoDB/Mongoose project.

---
+++ data/companies.js
@@ -0,0 +1,22 @@
+const companies = [
+    {
+        "id" : "cap",
+        "name" : "Capcom",
+        "country" : "Japan",
+        "active" : true
+    },
+    {
+        "id" : "lucarts",
+        "name" : "Lucasarts",
+        "country" : "United States",
+        "active" : false
+    },
+    {
+        "id" : "arc",
+        "name" : "Arc System Works",
+        "country" : "Japan",
+        "active" : true
+    }
+];
+
+export default companies;
---
+++ data/genres.js
@@ -0,0 +1,20 @@
+const genres = [
+    {
+        "id" : "act",
+        "name" : "action"
+    },
+    {
+        "id" : "rpg",
+        "name" : "Role-playing Game"
+    },
+    {
+        "id" : "strat",
+        "name" : "Strategy"
+    },
+    {
+        "id" : "adv",
+        "name" : "Adventure"
+    }
+];
+
+export default genres;
